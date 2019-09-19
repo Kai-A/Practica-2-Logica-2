f8abd51f1313c246e74842d3f141ca041853e83a
Kai-A <Kaikia01234@gmail.com>
2019-09-19 09:34:04 -0500

    Creación de la clase matriz y añadir el metodo llenar

--- src/Matriz.java
+++ src/Matriz.java
@@ -1,4 +1,33 @@
+import java.util.Random;
+
 public class Matriz {
-    
+    //Atributos privador de la clase matriz
+
+    private static  int matriz[][];
+
+//Constructor
+
+    public Matriz(int n) {
+        matriz= new int[n][n];
+    }
+
+    //Metodo de llenado de la matriz
+
+    public void Llenar(){
+        Random aleatorio = new Random();
+        for(int i=0; i<matriz.length; i++){
+            for(int j=0; j<matriz.length; j++) {
+                matriz[i][j] = aleatorio.nextInt(1000);
+            }
+        }
+    }
+    public void Mostrar(){
+        for(int i=0; i<matriz.length; i++){
+            for(int j=0; j<matriz.length; j++) {
+                System.out.println(matriz[i][j]);
+            }
+        }
+    }
+
 
 }
